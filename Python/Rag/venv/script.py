3b7dc068538e40467eb492a356b5d288baf9848b
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-07-03 00:10:44 -0600

    embedding y vector db

--- Python/Rag/venv/script.py
+++ Python/Rag/venv/script.py
@@ -1,5 +1,7 @@
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
 
 # Parte 1 Carga de data
 # 1. Carga de documento
@@ -11,5 +13,15 @@ text_splitter = RecursiveCharacterTextSplitter(
     chunk_size = 500,
     chunk_overlap = 50
 )
-
 splits = text_splitter.split_documents(docs)
+
+# 3. Embeddings con Ollama
+embeddings = OllamaEmbeddings(
+    model = "nomic-embed-text"
+)
+
+# 4. Crear vectordb (chroma)
+vectorstore = Chroma.from_documents(
+    documents = splits,
+    embedding = embeddings
+)

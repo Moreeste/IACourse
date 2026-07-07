f18c429444954ecdfd79a26e91c3ec902551dba8
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-07-06 23:50:31 -0600

    Update script.py

--- Python/Rag/venv/script.py
+++ Python/Rag/venv/script.py
@@ -2,6 +2,8 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
+from langchain_community.llms import Ollama
+from langchain.chains import RetrievalQA
 
 # Parte 1 Carga de data
 # 1. Carga de documento
@@ -25,3 +27,20 @@ vectorstore = Chroma.from_documents(
     documents = splits,
     embedding = embeddings
 )
+
+# Parte 2 Consulta
+#5. Modelo (LLM)
+llm = Ollama(
+    model = "qwen2.5:0.5b"
+)
+
+# 6. Cadena RAG
+qa = RetrievalQA.from_chain_type(
+    llm = llm,
+    retriever = vectorstore.as_retriever()
+)
+
+# 7. Pregunta
+query = "¿Cuál es la última tarea de la lista?"
+result = qa.invoke({"query": query})
+print(result["result"])

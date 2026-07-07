d07026d3311ea48ea772f2e1140676f6a3b8f9da
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-07-07 00:05:25 -0600

    Create ingest.py

---
+++ Python/Rag/venv/ingest.py
@@ -0,0 +1,31 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
+
+CHROMA_DIR = "./chroma_db"
+
+# 1. Carga de documento
+loader = TextLoader("document.txt", encoding="utf-8")
+docs = loader.load()
+
+# 2. Dividir en chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 50
+)
+splits = text_splitter.split_documents(docs)
+
+# 3. Embeddings con Ollama
+embeddings = OllamaEmbeddings(
+    model = "nomic-embed-text"
+)
+
+# 4. Crear vectordb (chroma)
+vectorstore = Chroma.from_documents(
+    documents = splits,
+    embedding = embeddings,
+    persist_directory = CHROMA_DIR
+)
+
+print(f"Ingestión completa. {len(splits)} chunks guardados en {CHROMA_DIR}.")

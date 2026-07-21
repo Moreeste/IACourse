ab1eecd0f2ce1088d4406c9b656a2f0def30aaf1
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-07-20 23:41:31 -0600

    inyección de dependencias

--- DotNet/Architecture/Components/Program.cs
+++ DotNet/Architecture/Components/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
 
-var xmlSave = new XMLSave("cervezas.xml");
-var program = new MyProgram(xmlSave);
+var services = new ServiceCollection();
+services.AddSingleton<ISave>(new XMLSave("cervezas.xml"));
+services.AddSingleton<MyProgram>();
+
+using var serviceProvider = services.BuildServiceProvider();
+var program = serviceProvider.GetRequiredService<MyProgram>();
 program.ShowMenu();
